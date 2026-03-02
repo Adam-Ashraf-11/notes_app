c9b867a0ae4733bfc9b6c3bee70a3dd6cd33a8d8
Adam Ashraf <adamashrafhamdy11@gmail.com>
2026-03-02 17:06:19 +0200

    make cus AppBar , SearchIcon

--- lib/View/notes_view.dart
+++ lib/View/notes_view.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notes/widget/custom_app_bar.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    
-    return Scaffold();
+    return Scaffold(
+   body: Padding(
+     padding: const EdgeInsets.symmetric(horizontal: 16),
+     child: Column(
+       children: [
+        SizedBox(height: 50,),
+         CustomAppBar(),
+       ],
+     ),
+   ),
+
+    );
+
+
+
   }
 }
+
+
